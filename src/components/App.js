777934987c8a98281f57ada28b5563529d731691
nikolai-sim <nikolai_sim@hotmail.com>
2022-04-04 11:32:10 +1200

    add route for signup

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import SignUp from "./SignUp";
 import { Container, container } from 'react-bootstrap'
 import { AuthProvider } from "./contexts/AuthContext";
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-    <AuthProvider>
-      <Container 
-      className="d-flex 
-      align-items-center 
-      justify-content-center" 
-      style={{minHeight: '100vh'}}>
-        <div className="w-100" style={{maxWidth: '400px'}}>
-          <SignUp/>
-        </div>
+    <Container 
+    className="d-flex 
+    align-items-center 
+    justify-content-center" 
+    style={{minHeight: '100vh'}}>
+      <div className="w-100" style={{maxWidth: '400px'}}>
+        <AuthProvider>
+        <Routes>
+            <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+        </AuthProvider>
+      </div>
       </Container>
-    </AuthProvider>
   );
 }
 
