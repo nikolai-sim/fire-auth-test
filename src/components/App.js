98400c3d3ca5da3178b71e444caaaa7fba69915e
nikolai-sim <nikolai_sim@hotmail.com>
2022-04-04 10:04:29 +1200

    add bootstrap form to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
+import SignUp from "./SignUp";
+import { Container, container } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      Shakka whaddup
-    </div>
+    <Container 
+    className="d-flex 
+    align-items-center 
+    justify-content-center" 
+    style={{minHeight: '100vh'}}>
+      <div className="w-100" style={{maxWidth: '400px'}}>
+        <SignUp/>
+      </div>
+    </Container>
+    
   );
 }
 
