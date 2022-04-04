6b6cb7660f9e35403d38b7aa6569638ec9127710
nikolai-sim <nikolai_sim@hotmail.com>
2022-04-04 12:54:32 +1200

    lint with prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
 import SignUp from "./SignUp";
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
-import Login from './Login'
-
+import Login from "./Login";
 
 function App() {
   return (
-    <Container 
-    className="d-flex 
+    <Container
+      className="d-flex 
     align-items-center 
-    justify-content-center" 
-    style={{minHeight: '100vh'}}>
-      <div className="w-100" style={{maxWidth: '400px'}}>
+    justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
         <AuthProvider>
-        <Routes>
-          <Route path='/' element={<Dashboard/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login/>}/>
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
         </AuthProvider>
       </div>
-      </Container>
+    </Container>
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
-import { useAuth } from './contexts/AuthContext'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Card, Button, Alert } from "react-bootstrap";
+import { useAuth } from "./contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
 
-  const [error, setError] = useState('')
-  const { currentUser, logout } = useAuth()
-
-  async function handleClick(){
-    setError('')
+  async function handleClick() {
+    setError("");
 
     try {
-      await logout()
-      navigate('/login')
+      await logout();
+      navigate("/login");
     } catch {
-      setError('Failed to Log Out')
+      setError("Failed to Log Out");
     }
-    }
+  }
 
   return (
     <>
-    <Card>
-      <Card.Body>
-      <h2 className='text-center mb-4'>Profile</h2>
-      {error && <Alert variant='danger'>{error}</Alert>}
-      <strong>Email: </strong>{currentUser?.email}
-      </Card.Body>
-    </Card>
-    <div className='w-100 text-center mt-2'>
-      <Button variant='link' onClick={handleClick}>Log Out</Button>
-    </div>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email: </strong>
+          {currentUser?.email}
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleClick}>
+          Log Out
+        </Button>
+      </div>
     </>
-  )
+  );
 }
