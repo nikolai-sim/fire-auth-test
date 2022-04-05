30916a8b05b26f86c9738c3f2dbd75a8090ae12b
nikolai-sim <nikolai_sim@hotmail.com>
2022-04-05 15:28:45 +1200

    add working reset for auth

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "./contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function ResetPassword() {
-  return <div>ResetPassword</div>;
+export default function Login() {
+  const navigate = useNavigate();
+
+  const emailRef = useRef();
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setError("");
+      setLoading(true);
+      await resetPassword(emailRef.current.value);
+      setSuccess(`A reset email has been sent to ${emailRef.current.value}`);
+    } catch {
+      setError("Failed to reset password");
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Reset Password</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          {success && <Alert variant="secondary">{success}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group>
+              <Button className="w-100 mt-2" type="submit" disabled={loading}>
+                Send Reset Email
+              </Button>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="wd-100 text-center mt-2">
+        Need an Account? <Link to="/signup">Sign Up</Link>
+      </div>
+    </>
+  );
 }
