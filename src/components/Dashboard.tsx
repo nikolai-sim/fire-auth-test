4de9ea54707445effc590b2aea363a5c35016817
nikolai-sim <nikolai_sim@hotmail.com>
2022-04-06 14:49:49 +1200

    remove unread imports

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
 import { useAuth } from "./contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
